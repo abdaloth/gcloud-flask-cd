e0f9a3c47c783947b65720f6d2f93df21dba6adb
abdaloth <abdullahoalothman@gmail.com>
2021-02-08 17:53:17 -0500

    changed landing page

--- main.py
+++ main.py
@@ -1,37 +1,14 @@
 from flask import Flask
-from flask import jsonify
+from flask import render_template
 import pandas as pd
-import wikipedia
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return '<h3>follow the light to find the truth</h3>\n<a href="/light">Light</a>'
-
-@app.route('/name/<value>')
-def name(value):
-    val = {"value": value}
-    return jsonify(val)
-
-@app.route('/html')
-def html():
-    """Returns some custom HTML"""
-    return """
-    <title>This is a Hello World World Page</title>
-    <p>Hello</p>
-    <p><b>World</b></p>
-    """
-@app.route('/light')
-def light():
-    return """<h1> A hot dog is a sandwich</h1>
-    
-    <img src='https://i.scdn.co/image/3f927022317b36fa20ecfdb589dec30f682600c2'>
-    """
-
-
+    """simple landing Page."""
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
