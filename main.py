4eea8122a90ecc0d141faf0646a0c4986b455e3f
abdaloth <abdullahoalothman@gmail.com>
2021-02-08 18:31:01 -0500

    renamed the html page

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import render_template
-import pandas as pd
 
 
 app = Flask(__name__)
@@ -8,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """simple landing Page."""
-    return render_template('hello.html')
+    return render_template('hello_gcp.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
